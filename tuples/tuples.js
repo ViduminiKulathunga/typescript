4972b14a5f81f495cd4c00d31ed5fe4604578f86
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-12-04 21:22:39 +0100

    tuples

---
+++ tuples/tuples.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var user = ["vidumini", 1];
+var users;
+users = ["Vidumini", 123, true];
+var rgb = [255, 255, 255];
+var newPerson = [1, "Vidumini"];
+newPerson[1] = "Amila";
---
+++ tuples/tuples.ts
@@ -0,0 +1,14 @@
+const user: (string | number)[] = ["vidumini", 1];
+
+let users: [string, number, boolean];
+users = ["Vidumini", 123, true];
+
+let rgb: [number, number, number] = [255, 255, 255];
+
+type User = [number, string];
+
+const newPerson: User = [1, "Vidumini"];
+newPerson[1] = "Amila";
+
+
+export{}
