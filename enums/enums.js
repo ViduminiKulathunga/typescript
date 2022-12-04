944ebb29210b5ef0aea9442f326cb7f00b554d2f
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-12-04 21:46:17 +0100

    enums

---
+++ enums/enums.js
@@ -0,0 +1,3 @@
+"use strict";
+exports.__esModule = true;
+var userSeat = 10 /* SeatChoice.ALISE */;
---
+++ enums/enums.ts
@@ -0,0 +1,9 @@
+const enum SeatChoice{
+    ALISE = 10,
+    MIDDLE,
+    WINDOW
+}
+
+const userSeat = SeatChoice.ALISE;
+
+export{}
