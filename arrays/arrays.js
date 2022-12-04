d0d63739e824c0d5b512725792de8467cbd499d9
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-12-04 18:38:34 +0100

    arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var superHeros = [];
+var heroPower = [];
+var allUsers = [];
+var MLModels = [[255, 255, 255], []];
+superHeros.push("Spiderman");
+heroPower.push(1);
+allUsers.push({ name: "Vidumini", isActive: true });
---
+++ arrays/arrays.ts
@@ -0,0 +1,16 @@
+const superHeros: string[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+const MLModels: number[][] = [[255, 255, 255], []];
+
+superHeros.push("Spiderman");
+heroPower.push(1);
+allUsers.push({ name: "Vidumini", isActive: true });
+
+export {};
