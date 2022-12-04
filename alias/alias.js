5b2e4ba4f8e7520aa026a9e00b40240440f9deb3
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-12-04 18:03:21 +0100

    alias readonly attributes and alias combination

--- alias/alias.js
+++ alias/alias.js
@@ -4,3 +4,10 @@ function createUser(user) {
     return { name: "", email: "", isActive: true };
 }
 createUser({ name: "", email: "", isActive: true });
+var myUser = {
+    _id: "1234",
+    name: "Vidu",
+    email: "vidu@gmail.com",
+    isActive: true
+};
+myUser.name = "Amila";
--- alias/alias.ts
+++ alias/alias.ts
@@ -4,10 +4,40 @@ type User = {
   isActive: boolean;
 };
 
+type Person = {
+    readonly _id: string;
+    name: string;
+    email: string;
+    isActive?: boolean;
+  };
+
+type cardNumber = {
+    cardnumber: string;
+}
+
+type cardDate = {
+    cardDate: string;
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number;
+}
+
 function createUser(user: User): User {
   return { name: "", email: "", isActive: true };
 }
 
 createUser({ name: "", email: "", isActive: true });
+
+let myUser: Person = {
+    _id: "1234",
+    name: "Vidu",
+    email: "vidu@gmail.com",
+    isActive: true
+}
+
+myUser.name = "Amila";
+
+let myAccount: cardDetails;
 
 export {}
