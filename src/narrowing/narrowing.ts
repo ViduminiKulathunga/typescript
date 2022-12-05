7899af5b80ae90b505a56de99defc5c7664a08f9
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-12-05 23:50:14 +0100

    narrowing

--- src/narrowing/narrowing.ts
+++ src/narrowing/narrowing.ts
@@ -59,3 +59,40 @@ function getFood(pet: Fish | Bird) {
     return "Bird Food";
   }
 }
+
+//discriminated unions
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+interface Rectangle {
+  kind: "reqtangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square;
+
+function getShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+    default:
+      const _defaultShape: never = shape;
+      return _defaultShape;
+  }
+}
