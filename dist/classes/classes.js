a70102622dae48b43792f21c18dc21dc3472d732
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-12-05 14:30:10 +0100

    public private modifiers, getter and setter

--- dist/classes/classes.js
+++ dist/classes/classes.js
@@ -4,6 +4,22 @@ class Person {
         this.email = email;
         this.name = name;
         this.city = "";
+        this._courseCount = 1;
+    }
+    get getEmail() {
+        return `Email apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set setCourseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error("Course count should be more than one!");
+        }
+        this._courseCount = courseNumber;
+    }
+    deleteToken() {
+        console.log("Token Deleted.");
     }
 }
 const person1 = new Person("vidu@gmail.com", "Vidumini");
--- src/classes/classes.ts
+++ src/classes/classes.ts
@@ -1,11 +1,27 @@
 class Person {
   private readonly city: string = "";
+  private _courseCount: number = 1;
 
-  constructor(
-    public email: string, 
-    public name: string){
+  constructor(public email: string, public name: string) {}
 
+  get getEmail(): string {
+    return `Email apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set setCourseCount(courseNumber: number) {
+    if (courseNumber <= 1) {
+      throw new Error("Course count should be more than one!");
     }
+    this._courseCount = courseNumber;
+  }
+
+  private deleteToken(): void {
+    console.log("Token Deleted.");
+  }
 }
 
 const person1 = new Person("vidu@gmail.com", "Vidumini");
