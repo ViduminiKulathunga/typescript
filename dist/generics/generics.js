7a3814fabe3f25574a4edcb20cc2c601c4dc734b
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-12-05 22:45:07 +0100

    generics

---
+++ dist/generics/generics.js
@@ -0,0 +1,17 @@
+"use strict";
+const score = [];
+const names = [];
+function indentityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+// identityThree("3");
+function identityFour(val) {
+    return val;
+}
+identityFour({ brand: "Zara", type: 1 });
